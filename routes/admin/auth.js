be9b5453aab95ee961f2c438163feb4640336bb3
Chris LeJeune <chrislejeune76@gmail.com>
2021-10-07 00:16:23 -0500

    error handling

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signup = require("../../views/admin/auth/signup");
 const signupTemplate = require("../../views/admin/auth/signup");
@@ -23,14 +23,9 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      //return signup template again
-      return res.send(signupTemplate({ req, errors }));
-    }
-    const { email, password, passwordConfirmation } = req.body;
-
+    const { email, password } = req.body;
     // create user in user repo to represent this person
     const user = await usersRepo.create({ email, password });
     // store id of user in the user's cookie
@@ -53,12 +48,8 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
     // check for user with this email
     const user = await usersRepo.getOneBy({ email });
