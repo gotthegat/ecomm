b7056033b1961780588bacf28b0adb79ee24a0ee
Chris LeJeune <chrislejeune76@gmail.com>
2021-10-08 00:11:10 -0500

    delete product

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -2,12 +2,16 @@ const { validationResult } = require("express-validator");
 const users = require("../../repositories/users");
 
 module.exports = {
-  handleErrors(templateFunc) {
-    return (req, res, next) => {
+  handleErrors(templateFunc, dataCb) {
+    return async (req, res, next) => {
       const errors = validationResult(req);
 
       if (!errors.isEmpty()) {
-        return res.send(templateFunc({ errors }));
+        let data = {};
+        if (dataCb) {
+          data = await dataCb(req);
+        }
+        return res.send(templateFunc({ errors, ...data }));
       }
 
       next(); // carry on if everything went well
